b29282a8004129b04ae1045f577b4e7efcaf80c4
Andrew Purcell <Andrew.Purcell@students.ittralee.ie>
2019-09-26 12:16:34 +0100

    Initial commit

---
+++ src/forLoop.java
@@ -0,0 +1,38 @@
+import javax.swing.JTextArea;
+public class forLoop {
+    public static void main(String[] args) {
+
+boolean valid = false;
+
+
+
+System.out.print("Yards\t\tInches\n");
+
+
+for(int i=1;i<=10;i++){
+    System.out.println(i + "\t" +(i * 36));
+
+    }//end loop
+
+        /**
+
+
+while(i<=10){
+    System.out.println(i + "\t" +(i * 36));
+    i++;
+}
+
+do{
+    System.out.println(i + "\t" +(i * 36));
+    i++;
+}while(i<=10);
+ */
+/**
+        while(valid == false){
+            System.out.println(i + "\t" +(i * 36));
+            i++;
+            valid = true;
+        }; */
+
+}
+ }
